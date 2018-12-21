c1abc539890cab7bec2d1d7955ad698b3316a357
Ajeet Singh <ajeet.singh.ec14@gmail.com>
2018-12-21 16:31:32 +0530

    Update and rename String_string_formatting to String_string_formatting.py

--- String_string_formatting.py
+++ String_string_formatting.py
@@ -1,6 +1,5 @@
 def print_formatted(number):
     w = len("{0:b}".format(number))
-
     for i in range(1, n+1):
         print("{0:{width}d} {0:{width}o} {0:{width}X} {0:{width}b}".format(i,width = w) )
 
