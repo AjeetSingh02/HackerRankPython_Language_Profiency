b4ed5342bdab8cb835827c42be2cf3048c7a27eb
Ajeet Singh <ajeet.singh.ec14@gmail.com>
2018-12-21 16:28:00 +0530

    Update and rename String_designer_door_mat to String_designer_door_mat.py

--- String_designer_door_mat.py
+++ String_designer_door_mat.py
@@ -1,18 +1,13 @@
 I = list(map(int,input().split(" ")))
 N,M = I[0],I[1]
-
 l = []
-
 for i in range(1,N,2):
-
     factor = int((M - 3*i)/2)
     temp = factor * "-" + ".|." * i + factor * "-"
     l.append(temp)
-    print(temp)
-        
+    print(temp)     
 factor = int((M-7)/2)
 print(factor * "-" + "WELCOME" + factor * "-")
-
 for _ in l[::-1]:
     print(_)
 
