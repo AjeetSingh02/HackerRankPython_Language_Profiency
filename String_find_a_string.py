134fc848645bf4a5959ded7b32fba011905294b4
Ajeet Singh <ajeet.singh.ec14@gmail.com>
2018-12-21 16:28:52 +0530

    Update and rename String_find_a_string to String_find_a_string.py

--- String_find_a_string.py
+++ String_find_a_string.py
@@ -4,12 +4,10 @@ def count_substring(string, sub_string):
     b = sub_string
     for i in range(0,len(a)-len(b)+1):
         l.append(a[i:i+len(b)])
-
     return l.count(b)
-
+    
 if __name__ == '__main__':
     string = input().strip()
     sub_string = input().strip()
-    
     count = count_substring(string, sub_string)
     print(count)
