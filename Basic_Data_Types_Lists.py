740756726f374d5623b72b71ebdff75f4cd0cd21
Ajeet Singh <ajeet.singh.ec14@gmail.com>
2018-12-21 10:12:41 +0530

    Update Basic_Data_Types_Lists.py

--- Basic_Data_Types_Lists.py
+++ Basic_Data_Types_Lists.py
@@ -2,8 +2,7 @@ if __name__ == '__main__':
     l = []
     for _ in range(int(input())):
         s = input().split()
-        cmd = s[0]
-        args = s[1:]
+        cmd, args = s[0], s[1:]
         if cmd !="print":
             cmd += "("+ ",".join(args) +")"
             eval("l."+cmd)
