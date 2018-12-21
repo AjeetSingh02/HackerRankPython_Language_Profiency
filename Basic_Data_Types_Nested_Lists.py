e3db87dfe3a876e3d671219d99f185c72dfab41b
Ajeet Singh <ajeet.singh.ec14@gmail.com>
2018-12-21 10:14:08 +0530

    Update and rename Basic_Data_Types_Nested_Lists to Basic_Data_Types_Nested_Lists.py

--- Basic_Data_Types_Nested_Lists.py
+++ Basic_Data_Types_Nested_Lists.py
@@ -1,19 +1,14 @@
 if __name__ == '__main__':
-    students = []
-    scores = []
+    students, scores = [], []
     for _ in range(int(input())):
-        name = input()
-        score = float(input())
+        name, score = input(), float(input())
         students.append([name,score])
         scores.append(score)
-
     sec_high_score = sorted(list(set(scores)))[1]
-
     names = []
     for data in students:
         if data[1] == sec_high_score:
             names.append(data[0])
-
     names = sorted(names)
     for name in names:
         print(name)
