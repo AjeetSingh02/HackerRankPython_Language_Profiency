00957da9278fd3d6ac7787d33599f8952db31b98
Ajeet Singh <ajeet.singh.ec14@gmail.com>
2018-12-21 12:45:41 +0530

    Update and rename Itertools_combinations_with_replacement to Itertools_combinations_with_replacement.py

--- Itertools_combinations_with_replacement.py
+++ Itertools_combinations_with_replacement.py
@@ -2,10 +2,7 @@ from itertools import combinations_with_replacement
 from functools import reduce
 inp = input().split(" ")
 string = sorted(inp[0])
-
 l = list(combinations_with_replacement(string,int(inp[1])))
-
 temp = [reduce(lambda x,y:x+y,tup) for tup in l]
-
 for ele in temp:
     print(ele)
